cf8998993590b028c85b98c5f556bafb7ce11c0c
Kartik Sharma <kartik.sharma1607@outlook.com>
2019-04-15 14:19:53 +0530

    Demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AComponent } from "./a/a.component";
+import { BComponent } from "./b/b.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: AComponent
+  },
+  {
+    path: "a",
+    component: AComponent
+  },
+  {
+    path: "b",
+    component: BComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AComponent,
+    BComponent
   ],
   imports: [
     BrowserModule,
